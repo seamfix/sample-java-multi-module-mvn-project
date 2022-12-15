b31fa5d81fea414655d181aa669a291f98295e1a
Chukwuemeka Chukwulobe <cchukwulobe@seamfix.com>
2022-12-15 12:37:12 +0100

    Addressed some sonar concerns

--- sub-application-1/src/main/java/com/example/calculator/Calculator.java
+++ sub-application-1/src/main/java/com/example/calculator/Calculator.java
@@ -1,4 +1,10 @@
+package com.example.calculator;
+
 public class Calculator {
+
+    private Calculator() {
+
+    }
 
     public static int addTwoNumbers(int a, int b) {
         return a + b;
--- sub-application-1/src/test/java/CalculatorTest.java
+++ sub-application-1/src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import com.example.calculator.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
